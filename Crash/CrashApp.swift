35aa9fb4744e09c555e317e2227756284724709d
Thomas Panis <thomas.panis@zappware.com>
2025-02-04 09:12:25 +0100

    Initial Commit

---
+++ Crash/CrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashApp.swift
+//  Crash
+//
+//  Created by Thomas Panis on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
